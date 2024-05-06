b8ed779a7ef225d56d35cdce3050dbbf2d6eafd9
Bizackk <chozito0205@gmail.com>
2024-05-06 16:47:26 -0500

    version-1

---
+++ departamentos.py
@@ -0,0 +1,34 @@
+import random
+
+Departamentos = {
+    'tolima' : 'ibague',
+    'cundinamarca' : 'bogota',
+    'antioquia' : 'medellin',
+    'bolivar' : 'cartagena',
+    'atlantico' : 'barranquilla'
+}
+
+while True:
+    print("Hola, soy python y voy a elegir un departamento y usted me dice la capital.")
+    print("-----------")
+    
+    departamento_azar = random.choice(list(Departamentos.keys()))
+    capital_correcta = Departamentos[departamento_azar]
+    
+    intentos = 3
+    
+    while intentos > 0:
+        pregunta = input(f"Cual es la capital de {departamento_azar}? Intentos restantes: {intentos}. Si desea irse tan solo escriba (salir): ")
+        
+        if pregunta.lower() == 'salir':
+            print("¡Hasta luego!")
+            exit()
+        elif pregunta.lower() == capital_correcta.lower():
+            print("¡Bien!")
+            exit()
+        else:
+            intentos -= 1
+            if intentos == 0:
+                print("¡Lo siento, has agotado tus intentos!")
+            else:
+                print("¡Incorrecto! Intenta de nuevo.")
